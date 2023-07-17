2e239bd086b01dad542f0ba72a6ba241c6b79f99
matheuscdd <matheuscdd@hotmail.com>
2023-07-17 06:17:09 -0300

    feat: finish

--- test.py
+++
@@ -1,9 +0,0 @@
-from navigator import browser
-from bs4 import BeautifulSoup as bs
-from requests import get
-from search_bible import search_web
-
-
-url = 'https://biblia.com/bible/nlt/john/6/35-38'
-res = get(url)
-print(search_web(res))
